1f132efb36b653cb9bd1210c0923f99ec6148f8b
nm1962028 <geistreichmeisenbach@outlook.com>
2015-01-21 09:40:03 -0800

    Signed-off-by: nm1962028 <geistreichmeisenbach@outlook.com>

--- Class_Lab/EtotheXwith2Functions/main.cpp
+++ Class_Lab/EtotheXwith2Functions/main.cpp
@@ -23,7 +23,7 @@ int main(int argc, char** argv)
     cin>>x;
     //compare the results
     cout<<"approximate e^x = "<<approxEx<<endl;
-    cout<<"Exact e^x = "<<exactEx<<endl;
+    cout<<"Exact e^x = "<<exp(x)<<endl;
     //exit stage right
     return 0;
 }
@@ -33,21 +33,26 @@ float approxEx(float x){
     //Calculate e^x
     for(int n=1;n<=13;n++)
         {
-        approxEx+=(pow(x,n)/nFactrl(n));
+        etox+=(pow(x,n)/nFactrl(n));
         }
     return etox;
 }
 
-int nFactrl(int n){
+int nFactrl(int n)
+{
     //Declare the variable
     //Note function only works for values 0-13
     int factorial=1;
     if(n==0||n==1)return factorial;
-    else if(n<=13){
-        for(int i=1;i<=n;i++){
+    else if(n<=13)
+    {
+        for(int i=1;i<=n;i++)
+        {
             factorial*=i;
         }
         return factorial;
-    }else{
+    }else
+    {
         return -1;
+    }
 }    
